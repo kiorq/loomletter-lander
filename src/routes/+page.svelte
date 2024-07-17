923388e052df4a5d844f2d9f268858f14694ac3a
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2024-07-17 10:38:00 -0600

    fixed dup h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -279,7 +279,7 @@
 <section class="w-full pb-[200px]">
 	<div class="flex flex-col gap-10 md:gap-20 items-center">
 		<div class="flex-grow flex flex-col gap-8 justify-start">
-			<h1 class="text-3xl -text-gradient font-bold text-center">Frequently Asked Questions</h1>
+			<p class="text-3xl -text-gradient font-bold text-center">Frequently Asked Questions</p>
 		</div>
 		<div class="w-full md:w-[1000px] shrink-0">
 			{#each faq as qa}
